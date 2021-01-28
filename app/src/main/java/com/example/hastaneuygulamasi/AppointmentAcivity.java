38c7c773c07576b82f08d3cbd4f18329a469fb9d
MuratKeremK <m.kerem.kara@gmail.com>
2021-01-28 22:39:18 +0300

    Hello World

---
+++ app/src/main/java/com/example/hastaneuygulamasi/AppointmentAcivity.java
@@ -0,0 +1,14 @@
+package com.example.hastaneuygulamasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_acivity);
+    }
+}
--- app/src/main/java/com/example/hastaneuygulamasi/MainActivity.java
+++ app/src/main/java/com/example/hastaneuygulamasi/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.hastaneuygulamasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,17 +13,20 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
-
+    Dialog epicDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseAuth = FirebaseAuth.getInstance();
+        epicDialog = new Dialog(this);
     }
     public void MakeAnAppointment(View view){
-        Intent intent = new Intent(MainActivity.this, FeedActivity.class);
-        startActivity(intent);
+      epicDialog.setContentView(R.layout.level_interface);
+      epicDialog.setCancelable(false);
+      epicDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+      epicDialog.show();
 
     }
 
