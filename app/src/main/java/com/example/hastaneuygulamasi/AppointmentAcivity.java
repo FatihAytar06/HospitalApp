5f0e1d109fa6d513c300c4c244eaf010497932a7
MuratKeremK <m.kerem.kara@gmail.com>
2021-01-29 16:11:27 +0300

    Hello World

--- app/src/main/java/com/example/hastaneuygulamasi/AppointmentAcivity.java
+++ app/src/main/java/com/example/hastaneuygulamasi/AppointmentAcivity.java
@@ -2,13 +2,30 @@ package com.example.hastaneuygulamasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.ref.Reference;
 
 public class AppointmentAcivity extends AppCompatActivity {
-
+    Intent intent;
+    String date;
+    TextView textView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference();
         setContentView(R.layout.activity_appointment_acivity);
+        intent = getIntent();
+        textView = findViewById(R.id.dateTextView);
+        date = intent.getStringExtra("date");
+        textView.setText(date);
     }
 }
--- app/src/main/java/com/example/hastaneuygulamasi/MainActivity.java
+++ app/src/main/java/com/example/hastaneuygulamasi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hastaneuygulamasi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
@@ -9,6 +10,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,7 +18,9 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     Dialog epicDialog;
-    Button closeButton;
+    Button closeButton, appointmentBtn;
+    CalendarView calendarView;
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +32,32 @@ public class MainActivity extends AppCompatActivity {
     public void MakeAnAppointment(View view){
         epicDialog.setContentView(R.layout.level_interface);
         closeButton = epicDialog.findViewById(R.id.closeButton);
-
+        appointmentBtn = epicDialog.findViewById(R.id.appointmentButton);
+        calendarView = epicDialog.findViewById(R.id.calendarView);
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                epicDialog.cancel();
+            }
+        });
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date = dayOfMonth + "/" + (month+1) + "/" + year ;
+            }
+        });
+        appointmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AppointmentAcivity.class);
+                intent.putExtra("date", date);
                 startActivity(intent);
             }
         });
       epicDialog.setCancelable(false);
       epicDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
       epicDialog.show();
-
-
     }
-
 
     public void AskQuestion(View view){
         Intent intent = new Intent(MainActivity.this, PickDoctor.class);
@@ -55,12 +69,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LogInScreen.class);
         startActivity(intent);
         finish();
-
     }
-    public void GoAccountInfo(View view){
 
+    public void GoAccountInfo(View view){
         Intent intent = new Intent(MainActivity.this, AccountActivity.class);
         startActivity(intent);
-
     }
 }
