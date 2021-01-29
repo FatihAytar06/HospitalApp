4cef800b263c1fd405216529e00bcf50a556aea9
MuratKeremK <m.kerem.kara@gmail.com>
2021-01-29 12:49:05 +0300

    Hello World

--- app/src/main/java/com/example/hastaneuygulamasi/MainActivity.java
+++ app/src/main/java/com/example/hastaneuygulamasi/MainActivity.java
@@ -8,12 +8,15 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     Dialog epicDialog;
+    Button closeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +26,24 @@ public class MainActivity extends AppCompatActivity {
         epicDialog = new Dialog(this);
     }
     public void MakeAnAppointment(View view){
-      epicDialog.setContentView(R.layout.level_interface);
+        epicDialog.setContentView(R.layout.level_interface);
+        closeButton = epicDialog.findViewById(R.id.closeButton);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
       epicDialog.setCancelable(false);
       epicDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
       epicDialog.show();
 
+
     }
+
 
     public void AskQuestion(View view){
         Intent intent = new Intent(MainActivity.this, PickDoctor.class);
